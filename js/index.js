7bb3d6ca718af7abd73c76d8c93af0768ead3129
NeeeeT <nailaznet7@gmail.com>
2021-01-20 21:58:09 +0800

    card flip added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+(function(){a={_b:0,c:function(){this._b++;return this.b;}};HTMLElement.prototype.pseudoStyle=function(d,e,f){var g="pseudoStyles";var h=document.head||document.getElementsByTagName('head')[0];var i=document.getElementById(g)||document.createElement('style');i.id=g;var j="pseudoStyle"+a.c();this.className+=" "+j;i.innerHTML+=" ."+j+":"+d+"{"+e+":"+f+"}";h.appendChild(i);return this;};})();
+
 let months = {
     1: [],
     2: [],
@@ -19,6 +21,7 @@ for(let i = 1; i <= 12; i++){
                 tempData = [];
                 tempData[0] = data[j].name;
                 tempData[1] = data[j].badThing1;
+                tempData[2] = data[j].picture1;
                 // months[i].push(data[j]);
                 months[i].push(tempData);   
             }
@@ -28,7 +31,7 @@ for(let i = 1; i <= 12; i++){
                 tempData = [];
                 tempData[0] = data[j].name;
                 tempData[1] = data[j].badThing2;
-
+                tempData[2] = data[j].pitcure2;
                 months[i].push(tempData);
         }
     }
